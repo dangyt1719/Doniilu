560ac44e3368057f5d195f0fe0260a39177fc997
dangyt1719 <38418619+dangyt1719@users.noreply.github.com>
2022-10-25 19:58:27 +0300

    Add files via upload

---
+++ NumbersPolicy/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumbersPolicy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите числа через пробел");
+
+            var arr = Array.ConvertAll(Console.ReadLine().Trim().Split(' '), Convert.ToInt32);
+
+            var result = arr.GroupBy(x => x).Where(w => w.Count() > 1).Select(s => s.Key).ToList();
+
+            var result2 = new HashSet<int>();
+
+            for(int i = 0; i < arr.Length; i++)
+            {
+                var exist = false;
+
+                for(int j=i+1; j< arr.Length; j++)
+                {
+                    if (arr[i] == arr[j])
+                    { 
+                        result2.Add(arr[i]);
+                        exist= true;
+                        break;
+                    }
+                }
+
+                if (exist) continue;
+            }
+
+            Console.WriteLine($"Повторяющиеся значения (алгоритм №1):{string.Join(" ", result)}");
+
+            Console.WriteLine($"Повторяющиеся значения (алогритм №2):{string.Join(" ", result2)}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ NumbersPolicy/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("NumbersPolicy")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NumbersPolicy")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("0dce6bf2-ecf2-4a6d-a57c-f2f150009808")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
